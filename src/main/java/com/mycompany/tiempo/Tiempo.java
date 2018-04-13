2df02c00870117c0e40cc0e93e20bdb4cbd0488b
asotodominguez <asotodominguez@victoria16.danielcastelao.org>
2018-04-13 09:33:52 +0200

    Obtengo los datos de la web.

--- src/main/java/com/mycompany/tiempo/Tiempo.java
+++ src/main/java/com/mycompany/tiempo/Tiempo.java
@@ -4,11 +4,28 @@ package com.mycompany.tiempo;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Tiempo {
 
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
+
+        String temperatura = "<span class=\"\">11<sup>°</sup></span>";
+        Document extraer2 = Jsoup.parseBodyFragment(temperatura);
+        Element body2 = doc.body();
+        System.out.println(temperatura);
+
+        String estado = "<div class=\"today_nowcard-phrase\">Parcialmente nublado</div>";
+        Document extraer = Jsoup.parseBodyFragment(estado);
+        Element body = doc.body();
+        System.out.println(estado);
+        
+        
+        
+        
+        
 
     }
     
