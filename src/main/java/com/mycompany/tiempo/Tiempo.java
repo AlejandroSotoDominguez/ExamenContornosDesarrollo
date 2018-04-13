3f42fcbd9532c8d0ea6b515e3d297fbc9919df75
asotodominguez <asotodominguez@victoria16.danielcastelao.org>
2018-04-13 09:51:35 +0200

    Muestro el título y la temperatura.

--- src/main/java/com/mycompany/tiempo/Tiempo.java
+++ src/main/java/com/mycompany/tiempo/Tiempo.java
@@ -7,25 +7,25 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
 public class Tiempo {
 
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
-
+        
         String temperatura = "<span class=\"\">11<sup>°</sup></span>";
-        Document extraer2 = Jsoup.parseBodyFragment(temperatura);
+        Document extraer = Jsoup.parseBodyFragment(temperatura);
         Element body2 = doc.body();
-        System.out.println(temperatura);
 
         String estado = "<div class=\"today_nowcard-phrase\">Parcialmente nublado</div>";
-        Document extraer = Jsoup.parseBodyFragment(estado);
+        Document extraer2 = Jsoup.parseBodyFragment(estado);
         Element body = doc.body();
+         
+        Document mostrarParseado = Jsoup.parse(temperatura);
+        System.out.println(doc.text());
+        
+        System.out.println(temperatura);
         System.out.println(estado);
-        
-        
-        
-        
-        
 
     }
     
